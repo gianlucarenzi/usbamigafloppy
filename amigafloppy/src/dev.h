f067704079d71130213c4aff69fc75949d81b674
Gianluca Renzi <gianlucarenzi@eurek.it>
2018-12-21 16:05:59 +0100

    - Added baudrate

--- amigafloppy/src/dev.h
+++ amigafloppy/src/dev.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef DEV_H_
 #define DEV_H_
 
-int init_device(const char *devname);
+int init_device(const char *devname, int baudrate);
 void shutdown_device(void);
 
 /* returns non-zero for success, zero for failure, and -1 on comm. error */
